649bc07e662666c9d844e4ae824c43cd783f1907
lequoctrung-it <leqtrungit@gmail.com>
2022-07-31 17:40:56 +0700

    Add sign in component and apply firebase login utils

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,9 +1,9 @@
-import {signInWithGoolePopup} from "../../utils/firebase/firebase.utils";
+import {createUserDocumentFromAuth, signInWithGoolePopup} from "../../utils/firebase/firebase.utils";
 
 function SignIn() {
   const logGoogleUser = async () => {
-    const response = await signInWithGoolePopup();
-    console.log(response)
+    const { user } = await signInWithGoolePopup();
+    const userDocRef = await createUserDocumentFromAuth(user)
   }
   return (
     <div>
--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,8 @@
 // Import the functions you need from the SDKs you need
 import {initializeApp} from "firebase/app";
 import {getAuth, signInWithRedirect, signInWithPopup, GoogleAuthProvider} from 'firebase/auth'
+import {getFirestore, doc, getDoc, setDoc} from "firebase/firestore"
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -25,3 +27,26 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGoolePopup = () => signInWithPopup(auth, provider);
+export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  const userSnapshot = await getDoc(userDocRef);
+
+  if (!userSnapshot.exists()) {
+    const { displayName, email } = userAuth;
+    const createAt = new Date();
+
+    try {
+      await setDoc(userDocRef, {
+        displayName,
+        email,
+        createAt
+      });
+    } catch (error) {
+      console.log('Error creating the user', error.message)
+    }
+  }
+
+  return userDocRef;
+}
