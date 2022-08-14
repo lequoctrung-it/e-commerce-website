f93b824f96284257f99373c5b452e4a3872d2464
lequoctrung-it <leqtrungit@gmail.com>
2022-08-14 07:28:04 +0700

    created authentication page component alongside sign in form

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import Home from "./routes/home/home.component";
-import {Route, Routes} from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const Shop = () => {
-  return <h1>This is the shop</h1>
-}
+  return <h1>This is the shop</h1>;
+};
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Navigation/>}>
-        <Route index element={<Home/>}/>
-        <Route path='shop' element={<Shop/>}/>
-        <Route path='sign-in' element={<SignIn/>}/>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,28 +1,28 @@
-import {Link, Outlet} from "react-router-dom";
-import {Fragment} from "react";
-import './navigation.styles.scss'
+import { Link, Outlet } from "react-router-dom";
+import { Fragment } from "react";
+import "./navigation.styles.scss";
 
-import {ReactComponent as CrwnLogo} from "../../assets/crown.svg";
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 
 function Navigation() {
   return (
     <Fragment>
-      <div className='navigation'>
-        <Link className='logo-container' to='/'>
-          <CrwnLogo className='logo' />
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className="logo" />
         </Link>
         <div className="nav-links-container">
-          <Link className='nav-link' to='/shop'>
+          <Link className="nav-link^" to="/shop">
             SHOP
           </Link>
-          <Link className='nav-link' to='/sign-in'>
+          <Link className="nav-link" to="/auth">
             SIGN IN
           </Link>
         </div>
       </div>
-      <Outlet/>
+      <Outlet />
     </Fragment>
-  )
+  );
 }
 
 export default Navigation;
